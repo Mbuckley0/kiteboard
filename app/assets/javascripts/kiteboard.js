c4602d0e7ca7d591c79d21544450c87220e7cfae
Mitchell Buckley <mitchellbuckley@Mitchells-MacBook-Pro.local>
2016-07-17 18:56:16 +1000

    Add initializing javascript

--- app/assets/javascripts/kiteboard.js
+++ app/assets/javascripts/kiteboard.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+$(function () {
+  'use strict';
+  window.Kiteboard = {
+    init: function () {
+      var contentWidth = 1240;
+      $('.gridster').width(contentWidth);
+      $('.gridster ul').gridster({
+        widget_margins: [5, 5],
+        widget_base_dimensions: [300, 360]
+      });
+    }
+  };
+});
