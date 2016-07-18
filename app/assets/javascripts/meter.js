359680da922ed5560fa189ca7a840cbce2b2dd0d
Mitchell Buckley <mitchellbuckley@Mitchells-MacBook-Pro.local>
2016-07-18 17:50:15 +1000

    Add meter widget

---
+++ app/assets/javascripts/meter.js
@@ -0,0 +1,12 @@
+$(function () {
+  'use strict';
+  window.Meter = {
+    setData: function (widget, data) {
+      widget.find(".meter").knob();
+
+      widget.find('.meter').val(data).trigger('change');
+
+      widget.find('.updated-at').html('Last updated at ' + new Date().toLocaleTimeString())
+    }
+  };
+});
