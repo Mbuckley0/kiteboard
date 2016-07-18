201ef155848e52a13cf6ad20a705727cafdb3598
Mitchell Buckley <mitchellbuckley@Mitchells-MacBook-Pro.local>
2016-07-18 20:27:16 +1000

    Add clock widget

---
+++ app/assets/javascripts/clock.js
@@ -0,0 +1,12 @@
+$(function () {
+  'use strict';
+  window.Clock = {
+    startClock: function (widget) {
+      setTimeout(function () {
+        widget.find('h1').html(new Date().toDateString());
+        widget.find('h2').html(new Date().toLocaleTimeString());
+        Clock.startClock(widget);
+      }, 500);
+    }
+  };
+});
