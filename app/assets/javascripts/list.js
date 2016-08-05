75f45477e017ed6295483614a7e777a2bb8af83f
Mitchell Buckley <mitchellbuckley@Mitchells-MacBook-Pro.local>
2016-08-06 09:30:49 +1000

    Update list widget to use an array of simple hashes

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -6,8 +6,8 @@ $(function () {
       list.empty();
       $.each(data, function(key, value){
         var li = $('<li/>').appendTo(list);
-        $('<span>' + value['label'] + '</span><span/>').addClass('label').appendTo(li);
-        $('<span>' + value['value'] + '<span/>').addClass('value').appendTo(li);
+        $('<span>' + key + '</span><span/>').addClass('label').appendTo(li);
+        $('<span>' + value + '<span/>').addClass('value').appendTo(li);
       });
 
       widget.find('.updated-at').html('Last updated at ' + new Date().toLocaleTimeString());
